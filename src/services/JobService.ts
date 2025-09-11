0065bda7951151a98f0f686198ad09908f89ee57
SarahSu92 <sarah.sundberg@outlook.com>
2025-09-11 11:03:52 +0200

    (refactor): change back to origin code

--- src/services/JobService.ts
+++ src/services/JobService.ts
@@ -2,21 +2,13 @@ import type { JobResult, JobDetail } from "../models/Jobs";
 
 
 // initial load
-export const getJobs = async (
-  query: string = "*",
-  offset = 0,
-  limit = 10
-): Promise<JobResult> => {
-  const params = new URLSearchParams({
-    q: query,
-    offset: offset.toString(),
-    limit: limit.toString(),
-  });
+export const getJobs = async () => {
+    const response = await fetch(
+        "https://jobsearch.api.jobtechdev.se/search?offset=0&limit=10"
+    );
+    const jobs: JobResult = await response.json();
 
-  const response = await fetch(`https://jobsearch.api.jobtechdev.se/search?${params}`);
-  if (!response.ok) throw new Error(`HTTP-fel ${response.status}`);
-
-  return response.json();
+    return jobs;
 };
 
 //job by id
