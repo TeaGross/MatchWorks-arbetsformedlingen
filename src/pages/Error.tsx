7b7f25da85fc1b8a13445da82e4960a903945d33
TeaGross <tea.grossman@gmail.com>
2025-09-11 17:08:32 +0200

    feat(ErrorPage): added content for errorpage with redirection to startpage

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,4 +1,21 @@
+import { ErrorPageStatusCodes } from "@digi/arbetsformedlingen"
+import { DigiLinkInternal, DigiNotificationErrorPage } from "@digi/arbetsformedlingen-react"
+
 export const Error = () => {
-    return <>
-    error</>
+    return (
+    <DigiNotificationErrorPage
+    afHttpStatusCode={ErrorPageStatusCodes.NOT_FOUND}
+    af-custom-heading="Hoppsan, något har blivit fel!">
+        <p slot="bodytext">Vi kan tyvärr inte hitta sidan du letar efter.</p>
+        <ul slot="links">
+            <li>
+            <DigiLinkInternal afHref="/" af-variation="small">
+                Till startsidan
+            </DigiLinkInternal>
+            </li>
+        </ul>
+    </DigiNotificationErrorPage>
+
+    )
+    
 }
