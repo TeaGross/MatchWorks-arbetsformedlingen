b1a80aa96753509bff1b0ead7f64c2d0f66bd5b0
SarahSu92 <sarah.sundberg@outlook.com>
2025-09-16 17:05:56 +0200

    (feat): added button to homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,18 +1,21 @@
+import { DigiLinkButton } from '@digi/arbetsformedlingen-react';
 import { FunFacts } from '../components/FunFacts';
 import { HeroSection } from '../components/HeroSection';
-
-
-
+import { LinkButtonSize, LinkButtonVariation } from '@digi/arbetsformedlingen';
 
 export const HomePage = () => {
- 
-
   return (
     <>
-    <HeroSection />
-      
-      <FunFacts/>
+      <HeroSection />
+      <DigiLinkButton
+        afHref="/JobPage"
+        afSize={LinkButtonSize.MEDIUM}
+        afVariation={LinkButtonVariation.PRIMARY}
+      >
+        Sök Jobb
+      </DigiLinkButton>
 
+      <FunFacts />
     </>
   );
 };
