9702eec7041962c5c8fa8c2230e0405f4a581640
SarahSu92 <sarah.sundberg@outlook.com>
2025-09-16 14:22:06 +0200

    (feat): completed header with menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,30 @@
-import { DigiHeader } from "@digi/arbetsformedlingen-react"
+import {
+  DigiHeader,
+  DigiHeaderNavigation,
+  DigiHeaderNavigationItem,
+} from '@digi/arbetsformedlingen-react';
 
 export const Header = () => {
-    return <>
-    <DigiHeader 
-    afSystemName="MatchWork"
-    
-    afHideSystemName= {false}
-    >
-    <a slot="header-logo" aria-label="Match startsida" href="/"></a>
-    
-    </DigiHeader>
+  return (
+    <>
+      <DigiHeader afSystemName="MatchWork" afHideSystemName={false}>
+        <DigiHeaderNavigation
+          afCloseButtonText="Stäng"
+          afCloseButtonAriaLabel="Stäng meny"
+          afNavAriaLabel="Huvudmeny"
+        >
+          <DigiHeaderNavigationItem afCurrentPage={true}>
+            <a href="/">Hem</a>
+          </DigiHeaderNavigationItem>
+          <DigiHeaderNavigationItem>
+            <a href="/">Jobb Sidan</a>
+          </DigiHeaderNavigationItem>
+          <DigiHeaderNavigationItem>
+            <a href="#footer">Kontakt</a>
+          </DigiHeaderNavigationItem>
+        </DigiHeaderNavigation>
+        <a slot="header-logo" className='logo' aria-label="Match startsida" href="/"></a>
+      </DigiHeader>
     </>
-
-}
+  );
+};
