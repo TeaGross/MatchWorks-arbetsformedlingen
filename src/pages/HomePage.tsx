712e9b148faa50594be1b2b759fe3f25a603dea7
TeaGross <tea.grossman@gmail.com>
2025-09-16 16:34:39 +0200

    feat(WhyMatchWork):added new component to Homepage
    -started homeJobSearch but couldnt finish, waiting for pagination refactoring
    -added whymatchWork-component with info.
    -grid in tablet & desktop

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,13 @@
 import { FunFacts } from '../components/FunFacts';
 import { HeroSection } from '../components/HeroSection';
+import { HomeJobSearch } from '../components/HomeJobSearch';
+import { WhyMatchWork } from '../components/WhyMatchWork';
 
 export const HomePage = () => {
   return (
     <>
+      <HomeJobSearch/>
+      <WhyMatchWork/>
       <HeroSection/>
       <FunFacts/>
 
