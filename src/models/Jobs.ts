71b811f99d8294e7467a3da316bcab925d9d251f
SarahSu92 <sarah.sundberg@outlook.com>
2025-09-18 11:57:19 +0200

    (hotfix): cleanup code

--- src/models/Jobs.ts
+++ src/models/Jobs.ts
@@ -1,23 +1,23 @@
 export type Job = {
-    id: string;
-    headline: string;
-    employer?: { name?: string };
-    workplace_address?: { municipality?: string; region?: string };
-    publication_date?: string;
-    application_deadline?: string;
-    webpage_url?: string;
-    AdDescription?: { text?: string };
+  id: string;
+  headline: string;
+  employer?: { name?: string };
+  workplace_address?: { municipality?: string; region?: string };
+  publication_date?: string;
+  application_deadline?: string;
+  webpage_url?: string;
+  AdDescription?: { text?: string };
 };
 
 type JobDescription = {
-    text?: string;
+  text?: string;
 };
 
 export type JobDetail = Job & {
-    description?: JobDescription;
+  description?: JobDescription;
 };
 
 export type JobResult = {
-    hits: Job[];
-    total: { value: number };
+  hits: Job[];
+  total: { value: number };
 };
